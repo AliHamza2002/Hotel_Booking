8101c9c3aa8a4f4fccdf33a2f198325e380a923d
AliHamza2002 <alihamzazubair@gmail.com>
2025-12-07 20:38:42 +0500

    Use fuzzy search for city filter

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -73,8 +73,12 @@ const AllRooms = () => {
 
       let response;
 
-      // If search parameters exist, use search endpoint
-      if (city || checkIn || checkOut || guests) {
+      // If city search, use fuzzy search
+      if (city && !checkIn && !checkOut && !guests) {
+        response = await roomAPI.searchRooms({ city });
+      }
+      // If search parameters exist, use booking search endpoint
+      else if (city || checkIn || checkOut || guests) {
         const params = {};
         if (city) params.city = city;
         if (checkIn) params.checkIn = checkIn;
